875a9d967696aa732e79dfdbfebf12b35986bb4a
Sakthi636 <sakthivelsaravanan636@gmail.com>
2023-09-05 19:57:43 +0530

    implementation of fron-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import  { Typography, AppBar} from "@material-ui/core";
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
 const App = () => {
   return (
     <div>
-        <AppBar position="static" color="inherit">
-            <Typography variant="h2" align="center">Video Chat</Typography>
-        </AppBar>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
